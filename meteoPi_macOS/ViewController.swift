1072a8ccc30557914bdffeacab9263d0b02c18b5
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-07-28 11:20:31 +0200

    Založení programu

---
+++ meteoPi_macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  meteoPi_macOS
+//
+//  Created by Lukáš Stankovič on 28.07.16.
+//  Copyright © 2016 Lukáš Stankovič. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
