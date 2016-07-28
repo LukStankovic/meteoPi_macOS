785551dbca3fb258deef2a84a41f3570e67d3d72
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-07-28 11:43:59 +0200

    Přidání Web View

--- meteoPi_macOS/ViewController.swift
+++ meteoPi_macOS/ViewController.swift
@@ -10,10 +10,12 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
 
     override var representedObject: AnyObject? {
